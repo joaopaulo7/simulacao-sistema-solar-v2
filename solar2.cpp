50932aa14fabc04c27a1a5a01db758f3600d5dec
João <joaopauloff7@hotmail.com>
2022-04-15 12:36:42 -0300

    Adição de antialising usando blend na função init

--- solar2.cpp
+++ solar2.cpp
@@ -82,6 +82,8 @@ void escreveRastro(Astro a){
 void init(void)
 {
    glEnable (GL_LINE_SMOOTH);
+   glEnable (GL_BLEND);
+   glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
    glHint (GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
    glLineWidth (1.5);
 }
